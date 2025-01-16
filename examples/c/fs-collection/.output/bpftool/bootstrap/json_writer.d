5e275bca335a74cbe75d3456629c0ce2beded384
lianyy <929169761@qq.com>
2025-01-16 22:13:07 +0800

    fs_watcher

--- examples/c/fs-collection/.output/bpftool/bootstrap/json_writer.d
+++
@@ -1,5 +0,0 @@
-/home/lyy/Desktop/fs-watcher/examples/c/fs-collection/.output/bpftool/bootstrap/json_writer.o: \
- json_writer.c json_writer.h \
- /home/lyy/Desktop/fs-watcher/bpftool/include/linux/compiler.h \
- /home/lyy/Desktop/fs-watcher/bpftool/include/linux/compiler_types.h \
- /home/lyy/Desktop/fs-watcher/bpftool/include/linux/compiler-gcc.h
